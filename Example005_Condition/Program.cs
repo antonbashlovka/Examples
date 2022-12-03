cc678442ba67081ff85f82218927ce070da91733
antonbashlovka <anton.bashlovka@gmail.com>
2022-12-03 13:04:31 +0500

    added condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша"){
+    Console.Write("Ура, это Маша");
+}else{
+    Console.Write("привет, " + username);
+}
+
