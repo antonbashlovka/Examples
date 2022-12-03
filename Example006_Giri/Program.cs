86af0b854f40f07855e14f636ea6e940e48ef6f5
antonbashlovka <anton.bashlovka@gmail.com>
2022-12-03 13:12:01 +0500

    added Giri

---
+++ Example006_Giri/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+int A = 6;
+int B = 8;
+int C = 5;
+int D = 3;
+int E = 7;
+
+int max = A;
+if (B > max) {max = B;}
+if (C > max) {max = C;}
+if (D > max) {max = D;}
+if (E > max) {max = E;}
+
+Console.WriteLine("max = " + max);
