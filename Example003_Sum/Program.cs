941d1ac70bc083d9a29d827f9f60e93daeefed52
antonbashlovka <anton.bashlovka@gmail.com>
2022-12-03 12:55:51 +0500

    added random numbers

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numA = 3;
-int numB = 4;
+﻿int numA = new Random().Next(1, 10);
+Console.WriteLine(numA);
+int numB = new Random().Next(1, 10);
+Console.WriteLine(numB);
 int result = numA + numB;
 Console.WriteLine(result);
